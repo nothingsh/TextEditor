22da5ce34ce6d2490dcbc19207c7e0be40fa5c0e
Steven Zhang <sn773636227@outlook.com>
2022-05-04 00:47:53 +0800

    Update RichTextEditor.swift
    add onCommit action

--- Sources/TextEditor/RichTextEditor.swift
+++ Sources/TextEditor/RichTextEditor.swift
@@ -7,15 +7,22 @@ public struct RichTextEditor: View {
     private let richText: NSMutableAttributedString
     private let placeholder: String
     private let accessorySections: Array<EditorSection>
+    private let onCommit: (NSAttributedString) -> Void
     
-    public init(richText: NSMutableAttributedString, placeholder: String = "Type ...", accessory sections: Array<EditorSection> = EditorSection.allCases) {
+    public init(
+        richText: NSMutableAttributedString,
+        placeholder: String = "Type ...",
+        accessory sections: Array<EditorSection> = EditorSection.allCases,
+        onCommit: @escaping ((NSAttributedString) -> Void)
+    ) {
         self.richText = richText
         self.placeholder = placeholder
         self.accessorySections = sections
+        self.onCommit = onCommit
     }
     
     public var body: some View {
-        TextEditorWrapper(richText: richText, height: $dynamicHeight, placeholder: placeholder, sections: accessorySections)
+        TextEditorWrapper(richText: richText, height: $dynamicHeight, placeholder: placeholder, sections: accessorySections, onCommit: onCommit)
             .frame(minHeight: dynamicHeight, maxHeight: dynamicHeight)
     }
 }
@@ -28,17 +35,23 @@ struct RichTextEditor_Previews: PreviewProvider {
     
     struct Preview: View {
         let richText: NSMutableAttributedString = NSMutableAttributedString()
+        @State var text = NSAttributedString(string: "Hello")
         
         var body: some View {
             ZStack {
                 Color(hex: "EED6C4")
                     .edgesIgnoringSafeArea(.all)
-                RichTextEditor(richText: richText)
+                VStack {
+                    RichTextEditor(richText: richText) { attributedString in
+                        self.text = attributedString
+                    }
                     .padding()
                     .background(
                         Rectangle().stroke(lineWidth: 1)
                     )
                     .padding()
+                    Text(text.string)
+                }
             }
         }
     }
